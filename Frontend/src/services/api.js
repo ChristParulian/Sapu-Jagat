0c24e6539c0c6afb1add5b91c84bec9cfbc5b9b2
Nur Aziz Raihan <nurazispbgp@gmail.com>
2025-06-04 19:53:09 +0700

    update login, register, update dashboard add daily check in, update bottom nav

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const API_URL = import.meta.env.VITE_API_URL
 
-export const register = async (username, password) => {
+export const register = async (username, email, password) => {
   try {
     const response = await axios.post(
       `${API_URL}/users`,
-      { username, password }
+      { username, email, password }
     )
 
     if (response.data.status === 'success') {
@@ -19,11 +19,11 @@ export const register = async (username, password) => {
   }
 }
 
-export const login = async (username, password) => {
+export const login = async (email, password) => {
   try {
     const response = await axios.post(
       `${API_URL}/login`,
-      { username, password }
+      { email, password }
     )
     if (response.data.status === 'success') {
       return response.data.data
@@ -32,5 +32,38 @@ export const login = async (username, password) => {
     }
   } catch (error) {
     throw error.response?.data?.message || error.message || 'Login gagal'
+  }
+}
+
+export const checkIn = async (token) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/checkin`,
+      {},
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+    if (response.data.status === 'success') {
+      return response.data
+    } else {
+      throw response.data.message || 'Check-in gagal'
+    }
+  } catch (error) {
+    throw error.response?.data?.message || error.message || 'Check-in gagal'
+  }
+}
+
+export const getCheckinHistoryByMonth = async (token, month) => {
+  try {
+    const response = await axios.get(
+      `${API_URL}/checkin/history/month?month=${month}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+    if (response.data.status === 'success') {
+      return response.data.data
+    } else {
+      throw response.data.message || 'Gagal mengambil riwayat check-in bulanan'
+    }
+  } catch (error) {
+    throw error.response?.data?.message || error.message || 'Gagal mengambil riwayat check-in bulanan'
   }
 }
